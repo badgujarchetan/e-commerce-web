df3ef753b662a64d50119864fd905f108bed3b1d
Chetan badgujar <badgujarchetan716@gmail.com>
2025-11-01 13:01:40 +0530

    add express.js middlware

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 const port = process.env.PORT || 3000;
 
@@ -11,5 +23,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server is listening on port  ${port} `);
+  console.log(`Server is listening on port ${port}`);
 });
