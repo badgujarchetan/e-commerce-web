6ea8776d7153ec0c8467c49475039286c30a5ab6
Chetan badgujar <badgujarchetan716@gmail.com>
2025-11-05 15:27:54 +0530

    updated code

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
+
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import ownerRouter from "./routes/ownerRouter.js";
+
+import connectDB from "./config/mongoose-connection.js";
+connectDB();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,15 +21,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
+
+app.use("/owner", ownerRouter);
+app.use("/product", productRouter);
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Connected successfully to server!");
+});
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("this is / port");
-});
-
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(` Server is listening on port ${port}`);
 });
